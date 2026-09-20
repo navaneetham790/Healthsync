d9668f3825f19a5ad2a4b5e8d6c4d6ef3ddbc02e
Navaneetha <navaneetham790@gmail.com>
2026-09-21 00:25:21 +0530

    fix(doctor): optimize drug interactions dataset bundling as JS module

--- HealthSync frontend/frontend/src/utils/drugInteractionEngine.js
+++ HealthSync frontend/frontend/src/utils/drugInteractionEngine.js
@@ -1,4 +1,4 @@
-import drugInteractionsDb from "../data/drugInteractions.json";
+import drugInteractionsDb from "../data/drugInteractionsData";
 
 // Clinical synonyms & brand name normalizer
 const BRAND_TO_GENERIC = {
